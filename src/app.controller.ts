f5bc6cd83fffd30b5643ba2fc5fbcbb778b08b70
luminuszz <luminuszz43@gmail.com>
2026-02-06 23:03:58 -0300

    fix: parse json error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,111 +3,95 @@ import { EnvService } from "@modules/env/env.service";
 import { IaPromptProvider } from "@modules/ia/providers/ia-prompt.provider";
 import { QueueProvider } from "@modules/queue/queue-provider";
 import { ClassNotificationBotService } from "@modules/telegram/bots/class-notification-bot.service";
-import {
-  Controller,
-  Get,
-  type OnModuleInit,
-  Post,
-  Query,
-} from "@nestjs/common";
-import {
-  HealthCheck,
-  HealthCheckService,
-  HttpHealthIndicator,
-  MemoryHealthIndicator,
-} from "@nestjs/terminus";
+import { Controller, Get, type OnModuleInit, Post, Query } from "@nestjs/common";
+import { HealthCheck, HealthCheckService, HttpHealthIndicator, MemoryHealthIndicator } from "@nestjs/terminus";
 import type { AxiosResponse } from "axios";
 import { Utils } from "./utils/parse-message";
 
 interface Notification {
-  name: string;
-  imageUrl: string;
-  chapter: number;
-  message: string;
-  url: string;
-  nextChapter: number;
-  workId: string;
-  chatId: string;
+	name: string;
+	imageUrl: string;
+	chapter: number;
+	message: string;
+	url: string;
+	nextChapter: number;
+	workId: string;
+	chatId: string;
 }
 
 export type TracingJobData = {
-  type: "errors" | "events" | "logs";
-  data: Error | unknown;
+	type: "errors" | "events" | "logs";
+	data: Error | unknown;
 };
 
 @Controller("/")
 export class AppController implements OnModuleInit {
-  constructor(
-    private readonly queue: QueueProvider,
-    private readonly telegramService: TelegramService,
-    private readonly healthCheckService: HealthCheckService,
-    private memory: MemoryHealthIndicator,
-    private http: HttpHealthIndicator,
-    private readonly envService: EnvService,
-    private readonly classNotificationBot: ClassNotificationBotService,
-  ) {}
+	constructor(
+		private readonly queue: QueueProvider,
+		private readonly telegramService: TelegramService,
+		private readonly healthCheckService: HealthCheckService,
+		private memory: MemoryHealthIndicator,
+		private http: HttpHealthIndicator,
+		private readonly envService: EnvService,
+		private readonly classNotificationBot: ClassNotificationBotService,
+	) {}
 
-  onModuleInit() {
-    void this.queue.subscribe<Notification>(
-      "SEND_TELEGRAM_NOTIFICATION",
-      (data) => this.sendUnreadWorkTelegramNotification(data),
-    );
+	onModuleInit() {
+		void this.queue.subscribe<Notification>("SEND_TELEGRAM_NOTIFICATION", (data) =>
+			this.sendUnreadWorkTelegramNotification(data),
+		);
 
-    void this.queue.subscribe<TracingJobData>("tracing", (data) =>
-      this.handleTracingJob(data),
-    );
-  }
+		void this.queue.subscribe<TracingJobData>("tracing", (data) => this.handleTracingJob(data));
+	}
 
-  async handleTracingJob(data: TracingJobData) {
-    if (data.type !== "errors") return;
+	async handleTracingJob(data: TracingJobData) {
+		if (data.type !== "errors") return;
 
-    const error = data.data as Error;
+		const error = data.data as Error;
 
-    await this.telegramService.sendMessage({
-      chatId: "5887244798",
-      message: `🚨 *Error Traced in workers* 🚨\n\n*Name:* ${error.name}\n*Message:* ${error.message}\n*Stack:* \`\`\`${error.stack}\`\`\``,
-    });
-  }
+		await this.telegramService.sendMessage({
+			chatId: "5887244798",
+			message: `🚨 *Error Traced in workers*
+        ${JSON.stringify(error)}
+      `,
+		});
+	}
 
-  async sendUnreadWorkTelegramNotification(notification: Notification) {
-    const { message, url, imageUrl, chatId } = notification;
+	async sendUnreadWorkTelegramNotification(notification: Notification) {
+		const { message, url, imageUrl, chatId } = notification;
 
-    const parsedMessage = Utils.parseTelegramMessage(
-      `${message.toString()}\n\n${url}`,
-    );
+		const parsedMessage = Utils.parseTelegramMessage(`${message.toString()}\n\n${url}`);
 
-    await this.telegramService.sendMessage({
-      message: parsedMessage,
-      imageUrl,
-      chatId,
-    });
-  }
+		await this.telegramService.sendMessage({
+			message: parsedMessage,
+			imageUrl,
+			chatId,
+		});
+	}
 
-  @Get("/debug-sentry")
-  getError() {
-    throw new Error("My first Sentry error!");
-  }
+	@Get("/debug-sentry")
+	getError() {
+		throw new Error("My first Sentry error!");
+	}
 
-  @HealthCheck()
-  @Get("health")
-  async healthCheck() {
-    return this.healthCheckService.check([
-      () => this.telegramService.healthCheck(),
-      () => this.queue.healthCheck() as any,
-      () => this.memory.checkHeap("memory_heap", 150 * 1024 * 1024),
-      () =>
-        this.http.responseCheck(
-          "okami_platform_integration",
-          `${this.envService.get("OKAMI_API_URL")}/health`,
-          (res: AxiosResponse<{ status: string }>) => res.data.status === "ok",
-        ),
-    ]);
-  }
+	@HealthCheck()
+	@Get("health")
+	async healthCheck() {
+		return this.healthCheckService.check([
+			() => this.telegramService.healthCheck(),
+			() => this.queue.healthCheck() as any,
+			() => this.memory.checkHeap("memory_heap", 150 * 1024 * 1024),
+			() =>
+				this.http.responseCheck(
+					"okami_platform_integration",
+					`${this.envService.get("OKAMI_API_URL")}/health`,
+					(res: AxiosResponse<{ status: string }>) => res.data.status === "ok",
+				),
+		]);
+	}
 
-  @Get("/classroom/daily")
-  async fetchClassroomDaily(@Query("weekDay") weekDay?: string) {
-    return this.classNotificationBot.getDailyClassByActiveSemester(
-      Number(weekDay),
-    );
-  }
+	@Get("/classroom/daily")
+	async fetchClassroomDaily(@Query("weekDay") weekDay?: string) {
+		return this.classNotificationBot.getDailyClassByActiveSemester(Number(weekDay));
+	}
 }
