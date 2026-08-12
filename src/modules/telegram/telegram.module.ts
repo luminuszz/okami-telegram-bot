e64e996258f26843561ff87f258e3675b76b61d3
daviribeiro <luminuszz43@gmail.com>
2026-08-12 12:20:03 -0300

    disble okami telegram bot service

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -1,26 +1,25 @@
-import { ClassNotificationBotService } from '@app/modules/telegram/bots/class-notification-bot.service';
-import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
-import { EnvModule } from '../env/env.module';
-import { OkamiModule } from '../okami/okami.module';
-import { TelegramService } from './bots/telegram.service';
+import { ClassNotificationBotService } from "@app/modules/telegram/bots/class-notification-bot.service";
+import { RememberRedmineBot } from "@modules/telegram/bots/remember-redmine-bot.service";
+import { Module } from "@nestjs/common";
+import { TerminusModule } from "@nestjs/terminus";
+import { EnvModule } from "../env/env.module";
+import { OkamiModule } from "../okami/okami.module";
+import { TelegramService } from "./bots/telegram.service";
 import {
-  telegrafClassNotificationProviderBot,
-  telegrafProvider,
-  telegrafRememberRedmineBotProvider,
-} from './providers/telegraf.provider';
-import { RememberRedmineBot } from '@modules/telegram/bots/remember-redmine-bot.service';
+	telegrafClassNotificationProviderBot,
+	telegrafProvider,
+	telegrafRememberRedmineBotProvider,
+} from "./providers/telegraf.provider";
 
 @Module({
-  imports: [EnvModule, OkamiModule, TerminusModule],
-  providers: [
-    telegrafProvider,
-    telegrafClassNotificationProviderBot,
-    telegrafRememberRedmineBotProvider,
-    TelegramService,
-    ClassNotificationBotService,
-    RememberRedmineBot,
-  ],
-  exports: [TelegramService, ClassNotificationBotService],
+	imports: [EnvModule, OkamiModule, TerminusModule],
+	providers: [
+		telegrafProvider,
+		telegrafClassNotificationProviderBot,
+		telegrafRememberRedmineBotProvider,
+		ClassNotificationBotService,
+		RememberRedmineBot,
+	],
+	exports: [TelegramService, ClassNotificationBotService],
 })
 export class TelegramModule {}
